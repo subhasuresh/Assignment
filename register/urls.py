06108a35149abd59d66666011ef74124f940faa2
root <root@11CPU0110L.amritavidya.edu>
2016-06-03 12:40:04 +0530

    msg

---
+++ register/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import include, url
+from register.views import *
+from register.models import *
+urlpatterns = [
+   url(r'^$', UserRegistrationView.as_view(), name='register_user'),
+
+  url(r'^user/success/', TemplateView.as_view(template_name='page.html'),
+     name='page')
+   ]
